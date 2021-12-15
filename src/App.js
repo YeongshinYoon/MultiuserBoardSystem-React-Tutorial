e124bfde20a00867a929c9d3f9edc85a01f5f4a1
YeongshinYoon <xenoshin777@naver.com>
2021-12-15 17:49:14 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import './App.css';
-import User from './components/User';
+import "./App.css";
+import React, { Component } from "react";
+import User from "./components/User";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const users = [
   {
@@ -28,26 +47,42 @@ const users = [
   }
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        users.map(user => {
-          return (
-            <User
-              key={user.id} //map을 사용할 때는 각 요소의 구분을 위해 key props를 추가
-              id={user.id}
-              image={user.image}
-              name={user.name}
-              birthday={user.birthday}
-              gender={user.gender}
-              job={user.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(user => {
+              return (
+                <User
+                  key={user.id} //map을 사용할 때는 각 요소의 구분을 위해 key props를 추가
+                  id={user.id}
+                  image={user.image}
+                  name={user.name}
+                  birthday={user.birthday}
+                  gender={user.gender}
+                  job={user.job}
+                />
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    )
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,19 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 class User extends React.Component {
     render() {
+        // 두 개 이상의 컴포넌트는 div로 묶어야
         return (
-            // 두 개 이상의 컴포넌트는 div로 묶어야
-            <div>
-                <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
